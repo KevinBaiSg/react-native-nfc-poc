673cafbd3a3676625fad8028ade5cdb9a3432f29
Kevin Bai <kevin.bai.sin@gmail.com>
2019-06-14 11:51:02 +0800

    fix

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NfcManager, {Ndef, NfcTech, ByteParser} from 'react-native-nfc-manager'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    NfcManager.isSupported()
+        .then(supported => {
+          this.setState({ supported });
+          if (supported) {
+            this._startNfc();
+          }
+        })
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
